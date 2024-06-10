346b5c8d2e8fd89efb13d7c4441861eab8194d1a
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2024-06-10 21:07:52 +0530

    variables and datatypes

---
+++ app/src/main/java/com/starkindustries/kotlinlanguagebasics/Kotlin Fundamentals.kt
@@ -0,0 +1,44 @@
+package com.starkindustries.kotlinlanguagebasics
+class Main
+{
+    companion object
+    {
+        @JvmStatic
+        fun main(args:Array<String>)
+        {
+            // Variables and datatypes
+
+            // Datatypes:
+            //Int,String,Char,Double,Float,Long,Boolean
+
+            // Variables:
+
+            // Deceleration:
+
+
+//            var a: Int
+//            var b:String
+//            var c:Float
+//            var d:Char
+//
+//            val a:Int
+//            val b:String
+//            val c:Float
+
+
+            // Initialization:
+
+            var a=10
+            var b="Aditya"
+            var c=true
+
+            val d='A'
+            val e=65.31
+            val f=false
+            println("The value of a is $a , The value of b is $b , The value of c is $c")
+            println("The value of d is $d , The value of e is $e , The value of f is $f")
+            println("I am Ironman")
+        }
+
+    }
+}
